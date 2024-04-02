6bedc707f5bf48422f62759fd873955077971c06
Waney Martins <waneymartins2003@hotmail.com>
2024-04-01 21:10:18 -0300

    add dropdown language

--- lib/app/modules/home/view/language_select_page.dart
+++ lib/app/modules/home/view/language_select_page.dart
@@ -1,11 +1,27 @@
+// ignore_for_file: deprecated_member_use
 import 'package:crud_flutter/app/utils/colors.dart';
+import 'package:crud_flutter/app/widgets/custom_animated_button.dart';
+import 'package:crud_flutter/l10n/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class LanguageSelectPage extends StatelessWidget {
+class LanguageSelectPage extends StatefulWidget {
   const LanguageSelectPage({super.key});
+
+  @override
+  State<LanguageSelectPage> createState() => _LanguageSelectPageState();
+}
+
+class _LanguageSelectPageState extends State<LanguageSelectPage> {
+  Locale? _selectedLanguage;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedLanguage = WidgetsBinding.instance.window.locale;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,8 +43,40 @@ class LanguageSelectPage extends StatelessWidget {
           ),
         ),
       ),
-      body: const Column(
-        children: [],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(
+              "Selecione abaixo o idioma de sua preferência:",
+              style:
+                  GoogleFonts.syne(fontSize: 16, fontWeight: FontWeight.w400),
+            ),
+            const SizedBox(height: 20),
+            DropdownButton<Locale>(
+              items: L10n.all.map<DropdownMenuItem<Locale>>((Locale value) {
+                return DropdownMenuItem<Locale>(
+                  value: value,
+                  child: Text(value.languageCode),
+                );
+              }).toList(),
+              onChanged: (Locale? locale) {
+                setState(() {
+                  _selectedLanguage = locale;
+                });
+              },
+            ),
+            const SizedBox(height: 40),
+            CustomAnimatedButton(
+              onTap: () async {},
+              widhtMultiply: 0.7,
+              height: 45,
+              colorText: white,
+              color: purple,
+              text: "Salvar",
+            ),
+          ],
+        ),
       ),
     );
   }
