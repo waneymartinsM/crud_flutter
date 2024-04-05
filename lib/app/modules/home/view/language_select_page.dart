fb18f4c61b883478d29f5e364e4b420cfbbbd8c2
Waney Martins <waneymartins2003@hotmail.com>
2024-04-05 00:11:09 -0300

    update dropdown

--- lib/app/modules/home/view/language_select_page.dart
+++ lib/app/modules/home/view/language_select_page.dart
@@ -2,11 +2,11 @@
 import 'package:crud_flutter/app/utils/colors.dart';
 import 'package:crud_flutter/app/widgets/custom_animated_button.dart';
 import 'package:crud_flutter/l10n/l10n.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:multi_dropdown/multiselect_dropdown.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LanguageSelectPage extends StatefulWidget {
@@ -20,6 +20,8 @@ class LanguageSelectPage extends StatefulWidget {
 
 class _LanguageSelectPageState extends State<LanguageSelectPage> {
   late Locale _selectedLanguage;
+  final List<String> items = ['Item1', 'Item2', 'Item3', 'Item4'];
+  String? selectedValue;
 
   @override
   void initState() {
@@ -76,41 +78,132 @@ class _LanguageSelectPageState extends State<LanguageSelectPage> {
               ),
             ),
             const SizedBox(height: 20),
-            DropdownButton<Locale>(
-              items: L10n.all.map<DropdownMenuItem<Locale>>((Locale value) {
-                IconData iconData;
-                switch (value.languageCode) {
-                  case 'en':
-                    iconData = Icons.flag_outlined;
-                    break;
-                  case 'pt':
-                    iconData = Icons.g_mobiledata;
-                    break;
-                  default:
-                    iconData = Icons.opacity;
-                }
+            // DropdownButton<Locale>(
+            //   items: L10n.all.map<DropdownMenuItem<Locale>>((Locale value) {
+            //     IconData iconData;
+            //     switch (value.languageCode) {
+            //       case 'en':
+            //         iconData = Icons.flag_outlined;
+            //         break;
+            //       case 'pt':
+            //         iconData = Icons.g_mobiledata;
+            //         break;
+            //       default:
+            //         iconData = Icons.opacity;
+            //     }
+            //
+            //     return DropdownMenuItem<Locale>(
+            //       value: value,
+            //       child: Row(
+            //         children: [
+            //           Icon(iconData),
+            //           SizedBox(width: 8),
+            //           Text(
+            //             value.languageCode.toUpperCase(),
+            //             style: TextStyle(fontSize: 16),
+            //           ),
+            //         ],
+            //       ),
+            //     );
+            //   }).toList(),
+            //   value: _selectedLanguage,
+            //   onChanged: (Locale? locale) {
+            //     if (locale != null) {
+            //       _updateLanguage(locale);
+            //     }
+            //   },
+            // ),
 
-                return DropdownMenuItem<Locale>(
-                  value: value,
-                  child: Row(
-                    children: [
-                      Icon(iconData),
-                      SizedBox(width: 8),
-                      Text(
-                        value.languageCode.toUpperCase(),
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ],
+            DropdownButtonHideUnderline(
+              child: DropdownButton2<Locale>(
+                isExpanded: true,
+                hint: Text(
+                  'Select Language',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Theme.of(context).hintColor,
                   ),
-                );
-              }).toList(),
-              value: _selectedLanguage,
-              onChanged: (Locale? locale) {
-                if (locale != null) {
-                  _updateLanguage(locale);
-                }
-              },
+                ),
+                items: L10n.all.map((Locale value) {
+                  IconData iconData;
+                  switch (value.languageCode) {
+                    case 'en':
+                      iconData = Icons.flag_outlined;
+                      break;
+                    case 'pt':
+                      iconData = Icons.g_mobiledata;
+                      break;
+                    default:
+                      iconData = Icons.opacity;
+                  }
+                  return DropdownMenuItem<Locale>(
+                    value: value,
+                    child: Row(
+                      children: [
+                        Icon(iconData),
+                        SizedBox(width: 8),
+                        Text(
+                          value.languageCode.toUpperCase(),
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  );
+                }).toList(),
+                value: _selectedLanguage,
+                onChanged: (Locale? locale) {
+                  if (locale != null) {
+                    _updateLanguage(locale);
+                  }
+                },
+                // buttonStyle: ButtonStyle(
+                //   padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                //     EdgeInsets.symmetric(horizontal: 16),
+                //   ),
+                //   height: MaterialStateProperty.all<double>(40),
+                //   width: MaterialStateProperty.all<double>(140),
+                // ),
+                // menuItemHeight: 40,
+              ),
             ),
+
+            //TODO: REMOVER DROPDOWN
+            // DropdownButton<Locale>(
+            //   items: L10n.all.map<DropdownMenuItem<Locale>>((Locale value) {
+            //     IconData iconData;
+            //     switch (value.languageCode) {
+            //       case 'en':
+            //         iconData = Icons.flag_outlined;
+            //         break;
+            //       case 'pt':
+            //         iconData = Icons.g_mobiledata;
+            //         break;
+            //       default:
+            //         iconData = Icons.opacity;
+            //     }
+            //
+            //     return DropdownMenuItem<Locale>(
+            //       value: value,
+            //       child: Row(
+            //         children: [
+            //           Icon(iconData),
+            //           SizedBox(width: 8),
+            //           Text(
+            //             value.languageCode.toUpperCase(),
+            //             style: TextStyle(fontSize: 16),
+            //           ),
+            //         ],
+            //       ),
+            //     );
+            //   }).toList(),
+            //   value: _selectedLanguage,
+            //   onChanged: (Locale? locale) {
+            //     if (locale != null) {
+            //       _updateLanguage(locale);
+            //     }
+            //   },
+            // ),
+
             const SizedBox(height: 40),
             CustomAnimatedButton(
               onTap: () async {
