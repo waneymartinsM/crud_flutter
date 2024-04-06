d13634594dcd9cb73c9194bccaff641fd08d4c95
Waney Martins <waneymartins2003@hotmail.com>
2024-04-05 23:48:58 -0300

    melhorias no codigo

--- lib/app/modules/home/view/language_select_page.dart
+++ lib/app/modules/home/view/language_select_page.dart
@@ -10,7 +10,10 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LanguageSelectPage extends StatefulWidget {
-  const LanguageSelectPage({super.key, required this.selectedLocale});
+  const LanguageSelectPage({
+    super.key,
+    required this.selectedLocale,
+  });
 
   final Locale selectedLocale;
 
@@ -20,8 +23,6 @@ class LanguageSelectPage extends StatefulWidget {
 
 class _LanguageSelectPageState extends State<LanguageSelectPage> {
   late Locale _selectedLanguage;
-  final List<String> items = ['Item1', 'Item2', 'Item3', 'Item4'];
-  String? selectedValue;
 
   @override
   void initState() {
@@ -51,100 +52,139 @@ class _LanguageSelectPageState extends State<LanguageSelectPage> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios_new_outlined, size: 20),
-          onPressed: () => Modular.to.navigate('/home'),
-        ),
+        leading: _buildArrowBack(),
         backgroundColor: purple,
         elevation: 0,
-        title: Text(
-          AppLocalizations.of(context)!.language,
-          style: GoogleFonts.syne(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
-          ),
-        ),
+        title: _buildTextLanguage(context),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Text(
-              AppLocalizations.of(context)!.selectPreferredLanguage,
-              style: GoogleFonts.syne(
-                fontSize: 16,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              decoration: BoxDecoration(
-                  color: greyLight, borderRadius: BorderRadius.circular(8)),
-              child: DropdownButtonHideUnderline(
-                child: DropdownButton2<Locale>(
-                  isExpanded: true,
-                  hint: Text( AppLocalizations.of(context)!.selectTheLanguage,
-                      style: GoogleFonts.syne(
-                          fontSize: 14, color: Theme.of(context).hintColor)),
-                  items: L10n.all.map((Locale value) {
-                    IconData iconData;
-                    switch (value.languageCode) {
-                      case 'en':
-                        iconData = Icons.flag_outlined;
-                        break;
-                      case 'pt':
-                        iconData = Icons.g_mobiledata;
-                        break;
-                      default:
-                        iconData = Icons.opacity;
-                    }
-                    return DropdownMenuItem<Locale>(
-                      value: value,
-                      child: Row(
-                        children: [
-                          Icon(iconData),
-                          const SizedBox(width: 5),
-                          Text(
-                            value.languageCode.toUpperCase(),
-                            style: GoogleFonts.syne(fontSize: 14),
-                          ),
-                        ],
-                      ),
-                    );
-                  }).toList(),
-                  value: _selectedLanguage,
-                  onChanged: (Locale? locale) {
-                    if (locale != null) {
-                      _updateLanguage(locale);
-                    }
-                  },
-                  dropdownStyleData: DropdownStyleData(
-                    padding: const EdgeInsets.symmetric(vertical: 6),
-                    decoration:
-                        BoxDecoration(borderRadius: BorderRadius.circular(8)),
-                    offset: const Offset(0, 8),
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 50),
-            CustomAnimatedButton(
-              onTap: () async {
-                SharedPreferences prefs = await SharedPreferences.getInstance();
-                await prefs.setString(
-                    'selectedLanguage', _selectedLanguage.languageCode);
-                Modular.to.pushNamed('/home/');
-              },
-              widhtMultiply: 0.7,
-              height: 45,
-              colorText: white,
-              color: purple,
-              text: AppLocalizations.of(context)!.save,
-            ),
-          ],
+      body: _buildBody(context),
+    );
+  }
+
+  IconButton _buildArrowBack() {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back_ios_new_outlined, size: 20),
+      onPressed: () => Modular.to.navigate('/home'),
+    );
+  }
+
+  Text _buildTextLanguage(BuildContext context) {
+    return Text(
+      AppLocalizations.of(context)!.language,
+      style: GoogleFonts.syne(
+          fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+    );
+  }
+
+  Padding _buildBody(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          _buildTextSelectLanguage(context),
+          const SizedBox(height: 20),
+          _buildDropdown(context),
+          const SizedBox(height: 50),
+          _buildButtonSave(context),
+        ],
+      ),
+    );
+  }
+
+  Text _buildTextSelectLanguage(BuildContext context) {
+    return Text(
+      AppLocalizations.of(context)!.selectPreferredLanguage,
+      style: GoogleFonts.syne(fontSize: 16, fontWeight: FontWeight.w400),
+    );
+  }
+
+  Container _buildDropdown(BuildContext context) {
+    var textStyle = GoogleFonts.syne(fontSize: 14);
+    return Container(
+      decoration: BoxDecoration(
+          color: greyLight, borderRadius: BorderRadius.circular(8)),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton2<Locale>(
+          isExpanded: true,
+          hint: Text(AppLocalizations.of(context)!.selectTheLanguage,
+              style: textStyle),
+          items: L10n.all.map((Locale value) {
+            String image;
+            switch (value.languageCode) {
+              case 'en':
+                image = 'assets/images/estados-unidos.png';
+                break;
+              case 'pt':
+                image = 'assets/images/brasil.png';
+                break;
+              case 'es':
+                image = 'assets/images/espanha.png';
+                break;
+              default:
+                image = 'assets/images/brasil.png';
+            }
+            return dropdownMenuItem(value, image, textStyle);
+          }).toList(),
+          value: _selectedLanguage,
+          onChanged: (Locale? locale) {
+            if (locale != null) {
+              _updateLanguage(locale);
+            }
+          },
+          dropdownStyleData: dropdownStyleData(),
         ),
       ),
     );
+  }
+
+  DropdownMenuItem<Locale> dropdownMenuItem(
+      Locale value, String image, TextStyle textStyle) {
+    return DropdownMenuItem<Locale>(
+      value: value,
+      child: Row(
+        children: [
+          Image.asset(image, height: 25),
+          const SizedBox(width: 10),
+          Text(translateLanguage(value.toString()), style: textStyle),
+        ],
+      ),
+    );
+  }
+
+  DropdownStyleData dropdownStyleData() {
+    return DropdownStyleData(
+      padding: const EdgeInsets.symmetric(vertical: 6),
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
+      offset: const Offset(0, 8),
+    );
+  }
+
+  CustomAnimatedButton _buildButtonSave(BuildContext context) {
+    return CustomAnimatedButton(
+      onTap: () async {
+        SharedPreferences prefs = await SharedPreferences.getInstance();
+        await prefs.setString(
+            'selectedLanguage', _selectedLanguage.languageCode);
+        Modular.to.pushNamed('/home/');
+      },
+      widhtMultiply: 0.7,
+      height: 45,
+      colorText: white,
+      color: purple,
+      text: AppLocalizations.of(context)!.save,
+    );
+  }
+
+  String translateLanguage(String languageCode) {
+    switch (languageCode) {
+      case 'pt':
+        return AppLocalizations.of(context)!.portuguese;
+      case 'en':
+        return AppLocalizations.of(context)!.english;
+      case 'es':
+        return AppLocalizations.of(context)!.spanish;
+      default:
+        return languageCode;
+    }
   }
 }
